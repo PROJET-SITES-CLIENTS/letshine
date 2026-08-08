da429220d45e94d33b20cc6c1b18dfbf87a7c415
EcolesPeretz <ecoles.pub@gmail.com>
2026-08-08 03:18:19 +0000

    Allow wildcard hostnames for Next.js image optimization

--- next.config.ts
+++ next.config.ts
@@ -9,8 +9,14 @@ const nextConfig: NextConfig = {
   reactStrictMode: false,
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "plus.unsplash.com" },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
     ],
   },
 };
